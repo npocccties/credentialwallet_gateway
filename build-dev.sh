3b8eb129b715b82dc8d2749a43b0bf1d09b649f1
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-08-08 17:36:29 +0900

    docker-compose.build.ymlの作成

--- build-dev.sh
+++ build-dev.sh
@@ -4,7 +4,8 @@ DIR=$(cd $(dirname $0); pwd)
 echo $DIR
 cd $DIR
 
-docker build --no-cache -t gateway .
+docker compose -f docker-compose.build.yml build --no-cache 
 docker container stop gateway 2>&1 || true
 docker run --rm -p 3000:3000 --name gateway --net app_network --detach gateway
+docker rmi deps bundler
 docker compose up -d
