af0e5bee0cfbe69e73cb4f8d934b20d2a8c2743d
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-09-28 04:41:16 +0000

    開発環境サーバーへのデプロイ用コンテナ定義を修正

--- script/build-dev.sh
+++ script/build-dev.sh
@@ -5,7 +5,7 @@ echo $DIR
 cd $DIR
 
 docker compose -f docker-compose.dev-server.yml build --no-cache 
-docker container stop gateway 2>&1 || true
-docker run --rm -p 3000:3000 --name gateway --net app_network --detach gateway
+docker container stop chilowallet 2>&1 || true
+docker run --rm -p 3000:3000 --name chilowallet --net app_network --detach chilowallet
 docker rmi deps bundler
 docker -f docker-compose.dev-server.yml up -d
