1c7abdab26fbe34c8f13588ea512b150ce99517e
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-10-12 02:37:55 +0000

    開発環境でprisma clientのインスタンスが複数作成されてしまう問題を対処

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient({
-  log: ["query", "error", "info", "warn"],
-});
+
+let prisma;
+
+// NOTE: https://github.com/prisma/prisma/issues/1983#issuecomment-620621213
+// NOTE: ホットリロード時にPrismaが新しいインスタンスを作成しないようにする。
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
+    log: ["query", "error", "info", "warn"],
+  });
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ["query", "error", "info", "warn"],
+    });
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
 
