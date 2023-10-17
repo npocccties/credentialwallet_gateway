cdefbf6497358a67f8b67ffce8e4bb14141b7747
t-koga-2023 <138557316+t-koga-2023@users.noreply.github.com>
2023-10-17 19:44:20 +0900

    Update create_table.sql MTG指摘（10/17）DBテーブル修正
    LMSリストテーブルに項目追加
      バッジ取得LMSアクセストークン
      バッジ取得LMSサービス名
    
    項目名修正
      mywallet ⇒ wallet
      customer ⇒ consumer
      owner ⇒ earner

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,31 +1,31 @@
-drop table if exists mywallets cascade;
+drop table if exists wallets cascade;
 drop table if exists badge_vcs cascade;
 drop table if exists badge_customers cascade;
 drop table if exists submissions cascade;
 drop table if exists lms_list cascade;
 -- ----------------------------------------------------------------------------------------------------
 
--- マイウォレット
-create table mywallets (
-    mywallet_id serial not null,     -- ウォレットID
+-- ウォレット
+create table wallets (
+    wallet_id serial not null,       -- ウォレットID
     orthros_id text not null,        -- オルトロスID
     created_at timestamp not null,   -- 作成日時
-    primary key (mywallet_id)
+    primary key (wallet_id)
 );
 
-create unique index on mywallets (
+create unique index on wallets (
     orthros_id
 );
 
 -- バッジVCテーブル
 create table badge_vcs (
     badge_vc_id serial not null,                -- バッジVC ID
-    mywallet_id integer not null,               -- マイウォレットID
+    wallet_id integer not null,                 -- ウォレットID
     lms_id integer not null,                    -- バッジ取得LMSの選択ID
     lms_name varchar(256)not  null,             -- バッジ取得LMSの選択表示名
     badge_uniquehash text not null,             -- バッジユニークハッシュ
     badge_name varchar(256) not null,           -- バッジ名
-    badge_owner_email text not null,            -- バッジ所有者EMail
+    badge_earner_email text not null,           -- バッジ獲得者EMail
     badge_class_id text not null,               -- バッジクラスID
     badge_issuer_name varchar(256) not null,    -- バッジ発行者名
     badge_issuedon timestamp not null,          -- バッジ発行日時
@@ -36,35 +36,35 @@ create table badge_vcs (
     created_at timestamp not null,              -- 作成日時
     deleted_at timestamp null,                  -- 削除日時
     primary key (badge_vc_id),
-    foreign key (mywallet_id) references mywallets (mywallet_id)
+    foreign key (wallet_id) references wallets (wallet_id)
 );
 
 create index on badge_vcs (
-    mywallet_id
+    wallet_id
 );
 
 -- 提出先
-create table badge_customers (
-    customer_id serial not null,                -- バッジ提出先ID
-    customer_name varchar(256) not null,        -- バッジ提出先名
+create table badge_consumers (
+    consumer_id serial not null,                -- バッジ提出先ID
+    consumer_name varchar(256) not null,        -- バッジ提出先名
     cabinet_url text not null,                  -- キャビネットURL
-    primary key (customer_id)
+    primary key (consumer_id)
 );
 
 -- 提出履歴
 create table submissions (
     badge_vc_id integer not null,               -- バッジVC ID
-    mywallet_id integer not null,               -- マイウォレットID
+    wallet_id integer not null,                 -- ウォレットID
     submited_at timestamp not null,             -- 提出日時
     submission_email text not null,             -- 提出EMAILアドレス
-    customer_id integer not null,               -- バッジ提出先ID
-    customer_name varchar(256) not null,        -- バッジ提出先名
+    consumer_id integer not null,               -- バッジ提出先ID
+    consumer_name varchar(256) not null,        -- バッジ提出先名
     primary key (badge_vc_id, submited_at),
     foreign key (badge_vc_id) references badge_vcs (badge_vc_id)
 );
 
 create index on submissions (
-    mywallet_id
+    wallet_id
 );
 
 -- LMSリスト
@@ -72,6 +72,8 @@ create table lms_list (
     lms_id serial not null,                     -- バッジ取得LMSの選択ID
     lms_name varchar(256) not null,             -- バッジ取得LMSの選択表示名
     lms_url text not null,                      -- バッジ取得LMSサイトURL
-    sso_enable boolean not null,                -- SSO可否
+    sso_enabled boolean not null,               -- SSO可否
+    lms_access_token text not null,             -- バッジ取得LMSアクセストークン
+    lms_servuce text not null,                  -- バッジ取得LMSサービス名
     primary key (lms_id)
 );
