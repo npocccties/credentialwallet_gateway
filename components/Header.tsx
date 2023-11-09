e4265548d60385f2ba9ecfa6b9cf7a2b69691f93
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-11-09 06:29:22 +0000

    update: ウォレット作成時のレイアウト修正、ログ出力の追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,15 @@ import NextLink from "next/link";
 import React from "react";
 import { FaUserAlt } from "react-icons/fa";
 
+import { orthrosUserGetters } from "@/share/store/loginUser/Orthros/main";
+
 type Props = {
   onOpen: () => void;
+  showContents: boolean;
 };
 
-export const Header: React.FC<Props> = ({ onOpen }) => {
-  // TODO: ログインユーザーの情報を取得する
-  const userName = "○○太郎";
+export const Header: React.FC<Props> = ({ onOpen, showContents = true }) => {
+  const { displayName } = orthrosUserGetters.useOrthrosUserData();
   return (
     <Box as="header" position={"fixed"} w={"100%"} zIndex={1000}>
       <Flex
@@ -20,28 +22,40 @@ export const Header: React.FC<Props> = ({ onOpen }) => {
         backgroundColor={"gray.200"}
         p={{ base: 8 }}
       >
-        <Box>
-          <HamburgerIcon w={6} h={6} cursor={"pointer"} onClick={() => onOpen()} />
+        <Box>{showContents && <HamburgerIcon w={6} h={6} cursor={"pointer"} onClick={() => onOpen()} />}</Box>
+        <Box
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <NextLink href="/">
+            <Link fontSize={{ base: "xl", md: "2xl" }} fontWeight={"bold"} style={{ textDecoration: "none" }}>
+              バッジウォレット
+            </Link>
+          </NextLink>
         </Box>
-        <NextLink href="/">
-          <Link fontSize={"2xl"} fontWeight={"bold"} style={{ textDecoration: "none" }}>
-            バッジウォレット
-          </Link>
-        </NextLink>
-        <Flex gap={"16px"} alignItems={"center"} display={{ base: "none", sm: "flex" }}>
-          <FaUserAlt />
-          <Text fontSize={"xl"}>{userName}</Text>
-        </Flex>
-        <Flex gap={"16px"} alignItems={"center"} display={{ base: "flex", sm: "none" }}>
-          <Menu>
-            <MenuButton cursor={"pointer"} minW={0} transition="all 1s">
-              <FaUserAlt />
-            </MenuButton>
-            <MenuList>
-              <MenuItem>{userName}</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
+        <Box>
+          {showContents && (
+            <>
+              <Flex gap={"16px"} alignItems={"center"} display={{ base: "none", sm: "flex" }}>
+                <FaUserAlt />
+                <Text fontSize={"xl"}>{displayName}</Text>
+              </Flex>
+              <Flex gap={"16px"} alignItems={"center"} display={{ base: "flex", sm: "none" }}>
+                <Menu>
+                  <MenuButton cursor={"pointer"} minW={0} transition="all 1s">
+                    <FaUserAlt />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>{displayName}</MenuItem>
+                  </MenuList>
+                </Menu>
+              </Flex>
+            </>
+          )}
+        </Box>
       </Flex>
     </Box>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ import { Metatag } from "@/components/Metatag";
 import { WaletVCList } from "@/components/page/wallet/List";
 import { SERVICE_NAME, SERVICE_DESCRITION } from "@/configs";
 import { pagePath } from "@/constants";
+import { errors } from "@/constants/error";
+import { logEndForPageSSR, logStartForPageSSR, logStatus } from "@/constants/log";
+import { loggerError, loggerInfo } from "@/lib/logger";
 import prisma from "@/lib/prisma";
 import { orthrosUserActions } from "@/share/store/loginUser/Orthros/main";
 
@@ -18,32 +21,45 @@ type Props = {
 };
 
 export async function getServerSideProps(context): Promise<GetServerSidePropsResult<Props>> {
+  loggerInfo(logStartForPageSSR(pagePath.wallet.list));
   // const eppn = context.req.headers["eppn"] as string;
   // const displayName = context.req.headers["displayName"] as string;
 
-  const eppn = "gonzaburo@nii.co.jp";
+  const eppn = "zaburo@nii.co.jp";
   const displayName = "テスト 三郎";
+
+  loggerInfo("context.req.headers", context.req.headers);
 
   if (!eppn || !displayName) {
     // TODO: Orthrosにリダイレクトする？
+    loggerError(`${logStatus.error} headers not found!`);
     throw new Error("ログイン情報が不正です。");
   }
 
-  const createdWallet = await prisma.wallet.findUnique({
-    where: {
-      orthrosId: eppn,
-    },
-  });
+  try {
+    const createdWallet = await prisma.wallet.findUnique({
+      where: {
+        orthrosId: eppn,
+      },
+    });
 
-  const isCreatedWallet = !createdWallet ? false : true;
+    const isCreatedWallet = !createdWallet ? false : true;
 
-  return {
-    props: {
-      eppn,
-      displayName,
-      isCreatedWallet,
-    },
-  };
+    loggerInfo(`${logStatus.success} ${pagePath.wallet.list}`);
+    return {
+      props: {
+        eppn,
+        displayName,
+        isCreatedWallet,
+      },
+    };
+  } catch (e) {
+    loggerError(`${logStatus.error} ${pagePath.wallet.list}`, e.message);
+
+    throw new Error(errors.response500.message);
+  } finally {
+    loggerInfo(logEndForPageSSR(pagePath.wallet.list));
+  }
 }
 
 const Home: NextPage<Props> = ({ eppn, displayName, isCreatedWallet }) => {
