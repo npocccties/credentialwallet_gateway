158a42058b7d80a033ef2a6364a94cb48ac3b2fa
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-11-10 08:18:55 +0000

    improve: 不要な再レンダリングを制御

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 import { Flex, Link } from "@chakra-ui/react";
-import React from "react";
+import React, { memo } from "react";
 
 const copyrightLink = process.env.NEXT_PUBLIC_COPYRIGHT_LINK as string;
 const copyright = process.env.NEXT_PUBLIC_COPYRIGHT as string;
 
-export const Footer: React.VFC = () => {
+export const Footer: React.VFC = memo(() => {
   return (
     <Flex minH={"64px"} alignItems={"center"} justifyContent={"center"} p={{ base: 4 }} gap={"16px"}>
       <Link href={copyrightLink} fontSize={"sm"} fontWeight={"medium"}>
@@ -12,4 +12,4 @@ export const Footer: React.VFC = () => {
       </Link>
     </Flex>
   );
-};
+});
