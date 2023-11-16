48e59abd91d9bd10c279dda82c55651a91a6f22d
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-16 11:46:37 +0900

    chore: ローカル実行を考慮

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isDevelopment = process.env.NODE_ENV == 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
   },
-  output: "wallet",
-  assetPrefix: "/wallet"
+  output: isDevelopment ? "wallet" : "",
+  assetPrefix: isDevelopment ? "/wallet" : ""
 };
 
 module.exports = nextConfig;
