0e75788490c768340556c963d2a95bc366cb943a
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-11-22 04:39:26 +0000

    fix: JWTの検証がtrueの時に不正アクセスエラーを表示しない

--- pages/login/error/index.page.tsx
+++ pages/login/error/index.page.tsx
@@ -1,11 +1,42 @@
 import { WarningIcon } from "@chakra-ui/icons";
 import { VStack, HStack, Box, Button, Text } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 import React from "react";
 
 import { Layout } from "@/components/Layout";
 import { Metatag } from "@/components/Metatag";
 import { SERVICE_DESCRITION, SERVICE_NAME } from "@/configs";
+import { pagePath } from "@/constants";
+import { errors } from "@/constants/error";
+import { logStartForPageSSR, logStatus, logEndForPageSSR } from "@/constants/log";
+import { loggerInfo, loggerError } from "@/lib/logger";
+import { verifyOrthrosJwt } from "@/lib/verifyJwt";
 import { useLogoutApi } from "@/share/usecases/logout/useLogoutApi";
+
+const page = pagePath.login.error;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  loggerInfo(logStartForPageSSR(page));
+
+  const jwt = req.cookies.jwt;
+
+  try {
+    const verify = await verifyOrthrosJwt(jwt);
+
+    // 認証情報が正常ならこのページを表示しない
+    if (verify) {
+      return { notFound: true };
+    }
+
+    loggerInfo(`${logStatus.success} ${page}`);
+    loggerInfo(logEndForPageSSR(page));
+
+    return { props: {} };
+  } catch (e) {
+    loggerError(`${logStatus.error} ${page}`, e.message);
+    throw new Error(errors.response500.message);
+  }
+};
 
 const index = () => {
   const loginPageUrl = process.env.NEXT_PUBLIC_LOGIN_PAGE_URL;
