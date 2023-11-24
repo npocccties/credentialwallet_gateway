84828495c8faedc72b3354fdd31526960cf13e59
t_koga <t_koga@dev-cabinet.oku.cccties.org>
2023-11-24 17:57:42 +0900

    e-Portforio向け取得済みバッチ一覧取得API 修正

--- pages/api/v1/user_badgelist/index.api.ts
+++ pages/api/v1/user_badgelist/index.api.ts
@@ -1,69 +1,48 @@
-import { Parser } from "xml2js";
-
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { errors } from "@/constants/error";
 import { logEndForApi, logStartForApi, logStatus } from "@/constants/log";
+import { getCookieValue } from "@/lib/cookie";
 import { loggerError, loggerInfo } from "@/lib/logger";
 import prisma from "@/lib/prisma";
+import { getUserInfoFormJwt } from "@/lib/userInfo";
 import { api } from "@/share/usecases/api";
 import { UserBadgeList } from "@/types/api/user_badgelist";
 
 const apiPath = api.v1.user_badgelist;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<UserBadgeList | {}>) {
+
   loggerInfo(logStartForApi(apiPath));
-  loggerInfo("request method", req.method);
-  loggerInfo("request body", req.body);
-
-  var SamlResponse_atob: string, SamlResponse_xml;
-  const { method, body } = req;
-  const { SamlResponse, saml } = body;
-
-  if (method !== "POST") {
-    loggerError(`${logStatus.error} bad request! not POST method`, req.method);
-
-    res.status(400).json({});
-    return;
-  }
-
-  //SAML値チェック関数呼び出し(認証OKの場合TRUEを返す)
-  //  [TODO]現状SAML対応されていないため保留とする
-  //const check = checkSamlResponse(SamlResponse);
-  const check = true;
-
-  //INPUT: reqOrthrosId
-  //  [TODO]本来の想定ではSAML値から抽出するが
-  //  現実装ではsamlパラメータに平文で入っている物とする
-  //  Walletとe-PortfolioををSAML対応する時に調整
-  const reqOrthrosId = saml;
-
-  //入力パラメータ不正の場合
-  if (!check || !reqOrthrosId) {
-    loggerError(`${logStatus.error} bad request!`, reqOrthrosId);
-
-    res.status(400).json({});
-    return;
-  }
-
-  //walletsテーブルに対応するorthrosIdが登録されているかチェック
-  const findWalletId = await prisma.wallet.findMany({
-    where: {
-      orthrosId: reqOrthrosId,
-    },
-  });
-
-  //console.log(findWalletId);
-
-  //対応するorthrosIdがない場合
-  if (!findWalletId || findWalletId.length <= 0) {
-    loggerError(`${logStatus.error} bad request! not found orthrosId`);
-
-    res.status(400).json({});
-    return;
-  }
 
   try {
+    const jwt = getCookieValue("jwt");
+    const { eppn } = getUserInfoFormJwt(jwt);
+    const orthrosId = eppn;
+
+    //eppnが取得できない場合
+    if(jwt == null || eppn == null) {
+      loggerError(`${logStatus.error} cannot get eppn in cookie`);
+
+      res.status(400).json({});
+      return;
+    }
+
+    //walletsテーブルに対応するorthrosIdが登録されているかチェック
+    const findWalletId = await prisma.wallet.findMany({
+      where: {
+        orthrosId: orthrosId,
+      },
+    });
+
+    //対応するorthrosIdがない場合
+    if (!findWalletId || findWalletId.length <= 0) {
+      loggerError(`${logStatus.error} bad request! not found orthrosId in DB`);
+
+      res.status(400).json({});
+      return;
+    }
+
     //badge_vcsテーブルからreqOrthrosIdに対応するバッジ情報を取得
     const findBadgeList = await prisma.badgeVc.findMany({
       select: {
@@ -85,34 +64,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   } finally {
     loggerInfo(logEndForApi(apiPath));
   }
-}
-
-function checkSamlResponse(SamlResponse) {
-  var SamlResponse_atob: string, SamlResponse_xml;
-
-  try {
-    SamlResponse_atob = atob(SamlResponse);
-    new Parser().parseString(SamlResponse_atob, (err, data) => {
-      if (err) {
-        console.log("Fail2");
-        console.log(err);
-      } else {
-        SamlResponse_xml = data;
-      }
-    });
-  } catch (e) {
-    console.log("Fail1");
-    console.log(e);
-  }
-
-  console.log(`SamlResponse: ${SamlResponse}`);
-  console.log(`SamlResponse_atob: ${SamlResponse_atob}`);
-  console.log("SamlResponse_xml: ");
-  console.log(SamlResponse_xml);
-
-  if (!SamlResponse_xml) {
-    return false;
-  }
-
-  return true;
 }
