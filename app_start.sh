c840849858da0299c769656aa5ca3517cfac434b
UenishiKeita <k_uenishi@intelligent-design.co.jp>
2023-12-15 09:27:38 +0000

    fix: build時の--no-cacheを削除

--- app_start.sh
+++ app_start.sh
@@ -9,6 +9,5 @@ if ! docker network ls | grep db_network > /dev/null 2>&1; then
 fi
 docker compose -f docker-compose.production-db.yml up -d
 docker container stop chilowallet 2>&1 || true
-docker compose -f docker-compose.production.yml build --no-cache
-docker compose -f docker-compose.production.yml up -d
+docker compose -f docker-compose.production.yml up -d --build
 /bin/sh ./server_db_restore.sh
